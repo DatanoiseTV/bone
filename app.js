6522d9bf4ce8f54d247b1c34ff932d6bc3bd0244
DatanoiseTV <github@ext.no-route.org>
2014-09-23 21:49:45 +0200

    xushush

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
         latlon.lat = position.coords.latitude;
         latlon.lon = position.coords.longitude;
-	latlon.battery = device.name;
+	latlon.battery = device.uuid;
         if(!position.coords.speed) { latlon.speed = 0; }
         else{ latlon.speed = position.coords.speed }
         
